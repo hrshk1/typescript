11d2560afd77d73212526c0b3cc92d11787b4671
hrshk1 <hrsh.110070391@gmail.com>
2026-02-08 23:15:37 +0530

    fix: correct typo in .gitignore and add type examples in typesints.ts

---
+++ src/typesints.ts
@@ -0,0 +1,8 @@
+let drink = "chai"
+// drink = 10 // error since drink is of type string
+let cup = Math.random()>0.5?10:"big"
+// cup is either a number or a string, so its type is number | string
+// we are not giving cup a specific type, so it is inferred as number | string. This is called type inferencing. Type inference means TypeScript automatically determines the type of a variable, function return, or expression based on how it’s used.
+
+let chaiFlavour:string = "masala"
+// here we are explicitly giving chaiFlavour a type of string. This is called type annotation. Type annotation means we explicitly specify the type of a variable, function parameter, or return value using a colon followed by the type.
