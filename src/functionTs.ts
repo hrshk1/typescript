5f4a325576a4810b15c27b23e94b01db8c81b328
hrshk1 <hrsh.110070391@gmail.com>
2026-02-24 18:24:13 +0530

    feat: add functions for chai preparation and order handling; include return type annotations for better type safety

--- src/functionTs.ts
+++ src/functionTs.ts
@@ -23,3 +23,19 @@ function orderSomeChai(type:string="masala"){}
 //in this way provide default value  
 
 //std practice is that we should write optional and default parameters at the end of the parameter list.
+
+function kuchbhi (){
+    if(Math.random() > 0.5){
+        return "Chai is ready!";
+    }
+    else return null;
+}
+//here the return type of the function is string | null because it can return either a string or null. TypeScript will infer the return type based on the possible return values of the function.
+
+function createChai(order:{
+    type:string;
+    sugar:number;
+    size:"small" | "medium" | "large";
+}){
+    console.log(`Creating a ${order.size} ${order.type} chai with ${order.sugar} sugar...`);
+}
